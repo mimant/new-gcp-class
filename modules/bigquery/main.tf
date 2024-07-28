97536ae814b6ceda316da70825c444b211518d5d
mimant <mimant10@gmail.com>
2024-07-28 15:17:42 +0000

    updated versions

--- modules/bigquery/main.tf
+++ modules/bigquery/main.tf
@@ -15,7 +15,7 @@
 
 module "bigquery" {
   source            = "terraform-google-modules/bigquery/google" # Path to the module
-  version           = "5.1.0" # Specify the version of the module you require
+  version           = "~> 2.0.0" # Specify the version of the module you require
   dataset_id        = "foo"
   dataset_name      = "foo"
   description       = "some description" # updated the description accordingly
--- modules/bigquery/outputs.tf
+++ modules/bigquery/outputs.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-output "dataset" {
-  value       = "${module.bigquery.dataset_id}"
+output "dataset_id" {
+  value       = module.bigquery.dataset_id
   description = "Unique id for the dataset being provisioned"
 }
