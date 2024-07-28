80cb4742ec922a74ff0cc08d56d94e7cf5977029
mimant <mimant10@gmail.com>
2024-07-28 16:39:06 +0000

    this is new commit

--- modules/bigquery/main.tf
+++ modules/bigquery/main.tf
@@ -1,27 +1,11 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 module "bigquery" {
   source            = "terraform-google-modules/bigquery/google" # Path to the module
   version           = "~> 2.0.0" # Specify the version of the module you require
   dataset_id        = "foo"
   dataset_name      = "foo"
-  dataset_labels    = {env = "dev"}
-  dataset_lables    = {env = "dev"}
   description       = "some description" # updated the description accordingly
-  project_id        = var.project
+  project_id        = var.project_id
   location          = "EU" # Update location if needed
   time_partitioning = "DAY"
+  dataset_labels    = {env = "dev"}
 }
